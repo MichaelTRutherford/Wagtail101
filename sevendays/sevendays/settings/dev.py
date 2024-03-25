be09648e648b90df3462c374131aa526bb5d1e5a
Michael Rutherford <mtruth65@gmail.com>
2024-03-25 14:46:35 +1000

    Completed 7 Days of Wagtail

---
+++ sevendays/sevendays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*^%0$1vb=g^z*#g+tl@1ydssbm09df3!)y)b0u9_f%_u&q8mnt"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
